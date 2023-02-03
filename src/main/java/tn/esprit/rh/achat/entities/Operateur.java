2d28930a7c492639a95fa956d43524e01b769227
alaeriahiesprit <119853340+alaeriahiesprit@users.noreply.github.com>
2023-02-03 17:43:23 +0100

    Add files via upload

---
+++ src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -0,0 +1,44 @@
+package tn.esprit.rh.achat.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//test comment
+//test comment 2
+//test comment 3
+//test comment 4
+//test comment 5
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Operateur implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idOperateur;
+	private String nom;
+	private String prenom;
+	
+	private String password;
+	@OneToMany
+	@JsonIgnore
+	private Set<Facture> factures;
+	
+}
